2d785eeeb4573745e9de87a8c18898ec0cc63eb6
Chixol <phw9278@gmail.com>
2024-04-19 16:50:40 +0900

    2024-04-19 sql 지정

--- DDL.sql
+++ DDL.sql
@@ -13,3 +13,27 @@
 # Relationship
 # 사용자 - 이메일 인증번호 : 이메일 인증번호 테이블에 등록된 이메일만 사용자의 이메일로 사용할 수 있음 (1 : 1)
 # 사용자 - 게시물 : 사용자가 게시물을 작성할 수 있음 (1 : N)
+
+# 사용자 (아이디(pk), 비밀번호, 이메일, 권한, 가입경로)
+# table name : user
+# user_id : VARCHAR(50) PRIMARY KEY
+# user_password : VARCHAR(255) NOT NULL 
+# user_email : VARCHAR(100) NOT NULL UNIQUE FOREIGN KEY email_auth_number(email)
+# user_role : VARCHAR(15) NOT NULL DEFAULT('ROLE_USER') CHECK('ROLE_USER','ROLE_ADMIN')
+# join_path : VARCHAR(5) NOT NULL DEFAULT('HOME') CHECK('HOME', 'KAKAO', 'NAVER')
+
+# 이메일 인증번호 (이메일,인증번호)
+# table name : email_auth_number
+# email : VARCHAR(100) PRIMARY KEY
+# auth_number : VARCHAR(4) NOT NULL 
+
+# 게시물 (접수번호(pk), 상태, 제목, 내용, 작성자(아이디), 작성일, 조회수, 답변)
+# table name : board
+# reception_number : INT PRIMARY KEY AUTO INCREMENT
+# status : BOOLEAN NOT NULL DEFAULT(false)
+# title : VARCHAR(100) NOT NULL
+# contents : TEXT NOT NULL
+# writer_id : VARCHAR(50) NOT NULL FOREIGN KEY user(user_id)
+# write_datetime : DATETIME NOT NULL DEFAULT(now())
+# view_count : INT NOT NULL DEFAULT(0)
+# comment : TEXT
