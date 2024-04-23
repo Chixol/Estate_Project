fcdebb3a2b410149a43be529ed4dfdc47bac2f84
Chixol <phw9278@gmail.com>
2024-04-23 13:28:14 +0900

    2024-04-23 12:00

--- DDL.sql
+++ DDL.sql
@@ -1,3 +1,4 @@
+-- Active: 1706776145758@@127.0.0.1@3306@estate
 # Entity
 # - 사용자
 # - 이메일 인증번호
@@ -37,3 +38,42 @@
 # write_datetime : DATETIME NOT NULL DEFAULT(now())
 # view_count : INT NOT NULL DEFAULT(0)
 # comment : TEXT
+
+
+
+## 데이터 베이스 생성
+CREATE DATABASE estate;
+USE estate;
+
+## 이메일 인증 번호 테이블 생성
+CREATE TABLE email_auth_number (
+    email VARCHAR(100) PRIMARY KEY,
+    auth_number VARCHAR(4) NOT NULL
+);
+
+CREATE TABLE user (
+    user_id VARCHAR(50) PRIMARY KEY,
+    user_password VARCHAR(255) NOT NULL,
+    user_email VARCHAR(100) NOT NULL UNIQUE,
+    user_role VARCHAR(15) NOT NULL 
+        DEFAULT('ROLE_USER') 
+        CHECK(user_role IN('ROLE_USER', 'ROLE_ADMIN')),
+    join_path VARCHAR(5) NOT NULL 
+        DEFAULT('HOME') 
+        CHECK(join_path IN('HOME', 'KAKAO', 'NAVER')),
+    CONSTRAINT user_email_fk 
+        FOREIGN KEY (user_email) REFERENCES email_auth_number(email)
+);
+
+CREATE TABLE board (
+    reception_number INT PRIMARY KEY AUTO_INCREMENT,
+    status BOOLEAN NOT NULL DEFAULT(false),
+    title VARCHAR(100) NOT NULL,
+    contents TEXT NOT NULL,
+    writer_id VARCHAR(50) NOT NULL,
+    write_datetime DATETIME NOT NULL DEFAULT(now()),
+    view_count INT NOT NULL DEFAULT(0),
+    comment TEXT,
+    CONSTRAINT writer_id_fk
+        Foreign Key (writer_id) REFERENCES user(user_id)
+);
