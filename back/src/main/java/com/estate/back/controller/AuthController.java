637162af0514c17ecfa2a376e337a3aa6c8d7c7a
Chixol <phw9278@gmail.com>
2024-04-25 12:08:14 +0900

    2024-04-25 10:00 인증번호관련 작업

--- back/src/main/java/com/estate/back/controller/AuthController.java
+++ back/src/main/java/com/estate/back/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.estate.back.dto.request.auth.EmailAuthRequestDto;
 import com.estate.back.dto.request.auth.IdCheckRequestDto;
 import com.estate.back.dto.response.ResponseDto;
 import com.estate.back.service.AuthService;
@@ -30,4 +31,11 @@ public class AuthController {
         return response;
     }
     
+    @PostMapping("/email-auth")
+    public ResponseEntity<ResponseDto> emailAuth (
+        @RequestBody @Valid EmailAuthRequestDto requestBody
+    ) {
+        ResponseEntity<ResponseDto> response = authService.emailAuth(requestBody);
+        return response;
+    }
 }
