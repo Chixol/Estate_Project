efebc3825ea450747fdce98b63eb2ed620432f6b
Chixol <phw9278@gmail.com>
2024-05-07 17:47:21 +0900

    2024-05-07 17:00 답글 API 작성

--- front/src/apis/board/index.ts
+++ front/src/apis/board/index.ts
@@ -1,13 +1,21 @@
 import axios from "axios";
-import { GET_BOARD_LIST_URL, GET_BOARD_URL, GET_SEARCH_BOARD_LIST_URL, INCREASE_VIEW_COUNT_URL, POST_BOARD_REQUEST_URL } from "src/constant";
+import { GET_BOARD_LIST_URL, GET_BOARD_URL, GET_SEARCH_BOARD_LIST_URL, INCREASE_VIEW_COUNT_URL, POST_BOARD_REQUEST_URL, POST_COMMENT_REQUEST_URL } from "src/constant";
 import { bearerAuthorization, requestErrorHandler, requestHandler } from "..";
 import ResponseDto from "../response.dto";
-import { PostBoardRequestDto } from "./dto/request";
+import { PostBoardRequestDto, PostCommentRequestDto } from "./dto/request";
 import { GetBoardListResponseDto, GetBoardResponseDto, GetSearchBoardListResponseDto } from "./dto/response";
 
 // function: Q&A 작성 API 함수
 export const postBoardRequest = async (requestBody: PostBoardRequestDto, accessToken: string) => {
     const result = await axios.post(POST_BOARD_REQUEST_URL, requestBody, bearerAuthorization(accessToken))
+        .then(requestHandler<ResponseDto>)
+        .catch(requestErrorHandler);
+    return result;
+};
+
+// function: Q&A 답글 작성 API 함수
+export const postCommentRequest = async (receptionNumber: number | string, requestBody: PostCommentRequestDto, accessToken: string) => {
+    const result = await axios.post(POST_COMMENT_REQUEST_URL(receptionNumber), requestBody, bearerAuthorization(accessToken))
         .then(requestHandler<ResponseDto>)
         .catch(requestErrorHandler);
     return result;
