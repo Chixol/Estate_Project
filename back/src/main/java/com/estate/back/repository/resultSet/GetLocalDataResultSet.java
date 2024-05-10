425b7e276032a5f56b91e4d4ea0da2728ba434c4
Chixol <phw9278@gmail.com>
2024-05-10 09:54:13 +0900

    2024-05-10 09:00 Qurey 작성

---
+++ back/src/main/java/com/estate/back/repository/resultSet/GetLocalDataResultSet.java
@@ -0,0 +1,8 @@
+package com.estate.back.repository.resultSet;
+
+public interface GetLocalDataResultSet {
+    String getTearMonth();
+    Integer getSale();
+    Integer getLease();
+    Integer gerMonthRent();
+}
--- back/src/main/java/com/estate/back/service/implementation/EstateServiceImplementation.java
+++ back/src/main/java/com/estate/back/service/implementation/EstateServiceImplementation.java
@@ -1,7 +1,13 @@
 package com.estate.back.service.implementation;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.estate.back.dto.response.ResponseDto;
+import com.estate.back.dto.response.estate.GetLocalDataResponseDto;
+import com.estate.back.entity.BoardEntity;
 import com.estate.back.repository.EstateRepository;
 import com.estate.back.service.EstateService;
 
@@ -12,5 +18,19 @@ import lombok.RequiredArgsConstructor;
 public class EstateServiceImplementation implements EstateService{
 
     private final EstateRepository estateRepository;
+
+    @Override
+    public ResponseEntity<? super GetLocalDataResponseDto> getLocalData(String local) {
+        
+        try {
+            
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetLocalDataResponseDto.success(null);
+
+    }
     
 }
