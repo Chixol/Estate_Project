4a2b3fc10ff8f79d256faec85bff763113e0d33f
Chixol <phw9278@gmail.com>
2024-05-10 15:16:03 +0900

    2024-05-10 14:00 Ratio front 작성

--- front/src/constant/index.ts
+++ front/src/constant/index.ts
@@ -50,6 +50,8 @@ export const SERVER_ESTATE_MODULE_URL = `${SERVER_API_URL}/estate`;
 
 export const GET_LOCAL_DATA_URL = (local: string) => `${SERVER_ESTATE_MODULE_URL}/local/${local}`;
 
+export const GET_RATIO_DATA_URL = (local: string) => `${SERVER_ESTATE_MODULE_URL}/ratio/${local}`
+
 // description: 게시물 상수 (n개씩 보기에는 해당 안되는 페이지라서 10개씩 지정)
 export const COUNT_PER_PAGE = 10;
 export const COUNT_PER_SECTION = 10;
