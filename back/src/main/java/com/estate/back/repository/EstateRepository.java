ab4ad0ba4ac28cc2526632295a07626187ef5931
Chixol <phw9278@gmail.com>
2024-05-10 13:35:07 +0900

    2024-05-10 12:00 Ratio api 생성

--- back/src/main/java/com/estate/back/repository/EstateRepository.java
+++ back/src/main/java/com/estate/back/repository/EstateRepository.java
@@ -2,6 +2,7 @@ package com.estate.back.repository;
 
 import java.util.List;
 
+import org.antlr.v4.runtime.atn.SemanticContext.AND;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import com.estate.back.entity.EstateEntity;
 import com.estate.back.repository.resultSet.GetLocalDataResultSet;
+import com.estate.back.repository.resultSet.GetRatioDataResultSet;
 
 @Repository
 public interface EstateRepository extends JpaRepository<EstateEntity, Integer>{
@@ -22,4 +24,21 @@ public interface EstateRepository extends JpaRepository<EstateEntity, Integer>{
     )
     List<GetLocalDataResultSet> getLocalData(@Param("local") String local);
 
+    @Query(value = 
+    "SELECT `year_month` as yearMonth, " +
+        "return40, return4060, return6085, return85, " +
+        "lease_ratio40 as leaseRatio40, " +
+        "lease_ratio4060 as leaseRatio4060, " +
+        "lease_ratio6085 as leaseRatio6085, " +
+        "lease_ratio85 as leaseRatio85, " +
+        "month_rent_ratio40 as monthRentRatio40, " +
+        "month_rent_ratio4060 as monthRentRatio4060, " +
+        "month_rent_ratio6085 as monthRentRatio6085, " +
+        "month_rent_ratio85 as monthRentRatio85 " +
+    "FROM estate " +
+    "WHERE `year_month` BETWEEN '2023-01-01' AND '2023-12-31' " +
+    "AND `local` = :local " ,
+    nativeQuery = true
+    )
+    List<GetRatioDataResultSet> getRatioData(@Param("local") String local);
 }
